738cfe4dc889850e452d56dde87c43565b5a6010
charlesmarshall <cm56marshall@googlemail.com>
2011-10-28 11:12:46 +0100

    contact & callback

--- lib/model/WildfireCallback.php
+++ lib/model/WildfireCallback.php
@@ -1,9 +1,17 @@
 <?
-class WildfireCallback extends WildfireSubscribe{
+class WildfireCallback extends WaxModel{
   
   public function setup(){
-    parent::setup();
-    $this->define("telephone", "CharField", array('scaffold'=>true));
+    $this->define("name", "CharField", array('scaffold'=>true));
+    $this->define("telephone", "EmailField", array('scaffold'=>true, 'required'=>true));
+    $this->define("from_page", "CharField", array('editable'=>false));
+    $this->define("ip", "CharField", array('editable'=>false));
+    $this->define("occurred", "CharField", array('editable'=>false, 'scaffold'=>true));
+  }
+  
+  public function before_save(){
+    if(!$this->ip) $this->ip = $_SERVER['REMOTE_ADDR'];
+    if(!$this->occurred) $this->ip = $_SERVER['REMOTE_ADDR'];
   }
 }
 ?>
