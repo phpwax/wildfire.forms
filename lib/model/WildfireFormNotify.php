b2ca49a83e6031de5cc60a9567ae3f74148db6e0
charlesmarshall <cm56marshall@googlemail.com>
2011-10-28 12:40:34 +0100

    fixes to base model

--- lib/model/WildfireFormNotify.php
+++ lib/model/WildfireFormNotify.php
@@ -1,7 +1,7 @@
 <?
 class WildfireFormNotify extends WaxEmail {
-  public $from = ""
-  public $from_name = ""
+  public $from = "";
+  public $from_name = "";
   public $dev_emails = array("dev@oneblackbear.com");
   
 
@@ -16,9 +16,9 @@ class WildfireFormNotify extends WaxEmail {
   }
 
   protected function setup($data, $config){
-    foreach($config as $k=>$v) $this->$v = $v;  
+    foreach($config as $k=>$v) if($k != "class" && $k != "action") $this->$k = $v;  
     foreach($this->dev_emails as $e) $this->add_bcc_address($e);
-    $this->data = $record;
+    $this->data = $data->row;
   }
 
 }?>
