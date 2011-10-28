606e9e64e60900b34d792ee376af6ad448613475
charlesmarshall <cm56marshall@googlemail.com>
2011-10-28 12:04:41 +0100

    register the subscription module

--- setup.php
+++ setup.php
@@ -1,3 +1,4 @@
 <?
 CMSApplication::register_module("contact", array("display_name"=>"Contact Requests", "link"=>"/admin/contact/"));
+CMSApplication::register_module("subscribe", array("display_name"=>"Subscriptions", "link"=>"/admin/subscribe/"));
 ?>
