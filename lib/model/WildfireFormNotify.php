d7e6b03c2a766849132bb89ebf46d2bf60a5b201
charlesmarshall <cm56marshall@googlemail.com>
2011-10-28 12:19:20 +0100

    notify class

---
+++ lib/model/WildfireFormNotify.php
@@ -0,0 +1,24 @@
+<?
+class WildfireFormNotify extends WaxEmail {
+  public $from = ""
+  public $from_name = ""
+  public $dev_emails = array("dev@oneblackbear.com");
+  
+
+  public function contact($record, $config){
+    $this->setup($record, $config);
+  }
+  public function callback($record, $config){
+    $this->setup($record, $config);
+  }
+  public function subscribe($record, $config){
+    $this->setup($record, $config);
+  }
+
+  protected function setup($data, $config){
+    foreach($config as $k=>$v) $this->$v = $v;  
+    foreach($this->dev_emails as $e) $this->add_bcc_address($e);
+    $this->data = $record;
+  }
+
+}?>
